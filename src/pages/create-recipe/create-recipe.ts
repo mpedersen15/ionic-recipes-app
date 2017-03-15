70be4724e3c60afb41112df011c08bfb6972947d
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-03-15 09:22:39 -0600

    added functionality to create-recipe action sheet, add ingredient prompt; added toasts

--- src/pages/create-recipe/create-recipe.ts
+++ src/pages/create-recipe/create-recipe.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { NavController,	NavParams,	ActionSheetController, AlertController } from 'ionic-angular';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { NavController,	NavParams,	ActionSheetController, AlertController, ToastController } from 'ionic-angular';
 
  @ Component({
 	selector: 'page-create-recipe',
@@ -16,11 +16,12 @@ export class CreateRecipePage implements OnInit{
 	difficulty: string = "easy";
 
 	constructor( 	
-					public navCtrl: NavController, 
-					public navParams: NavParams, 
-					public actionSheetCtrl: ActionSheetController, 
-					public alertCtrl: AlertController
-				) {}
+		public navCtrl: NavController, 
+		public navParams: NavParams, 
+		public actionSheetCtrl: ActionSheetController, 
+		public alertCtrl: AlertController,
+		public toastCtrl: ToastController
+	) {}
 
 	onManageIngredients() {
 		console.log('adding ingredients');
@@ -32,13 +33,36 @@ export class CreateRecipePage implements OnInit{
 						text: 'Add Ingredient',
 						handler: () => {
 							console.log('Add ingredient clicked');
-							this.showIngredientPromt();
+							
+							this.createIngredientPrompt().present()
+							
+							//this.showIngredientPromt();
 						}
 					}, {
 						text: 'Remove All Ingredients',
 						role: 'destructive',
 						handler: () => {
 							console.log('Remove all ingredients clicked');
+							
+							const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
+							
+							const len = fArray.length;
+							
+							if (len > 0){
+								for (let i = len - 1; i >= 0; i--){
+									fArray.removeAt(i);
+								}
+								
+								const toast = this.toastCtrl.create({
+									message: 'All ingredients removed',
+									duration: 1000,
+									position: 'bottom'
+								});
+								
+								toast.present();
+								
+							}
+							
 						}
 					}, {
 						text: 'Cancel',
@@ -53,31 +77,52 @@ export class CreateRecipePage implements OnInit{
 		console.log('trying to add recipe');
 	}
 
-	private showIngredientPromt() {
-		let prompt = this.alertCtrl.create({
-				title: 'Add Ingredients',
-				message: "What would you like to add?",
-				inputs: [
-					{
-						name: 'ingredient',
-						placeholder: 'Ingredient'
-					},
-				],
-				buttons: [{
-						text: 'Cancel',
-						role: 'cancel'
-					}, {
-						text: 'Add',
-						handler: data => {
-							console.log('Add ingredient', data);
-							if (data.name.trim() === '' || data.name === null){
-								// show toast to enter a valid ingredient
-							}
+	private createIngredientPrompt() {
+		return this.alertCtrl.create({
+			title: 'Add Ingredients',
+			message: "What would you like to add?",
+			inputs: [
+				{
+					name: 'ingredient',
+					placeholder: 'Ingredient'
+				},
+			],
+			buttons: [{
+					text: 'Cancel',
+					role: 'cancel'
+				}, {
+					text: 'Add',
+					handler: data => {
+						console.log('Add ingredient', data);
+						if (data.ingredient.trim() === '' || data.ingredient === null){
+							// show toast to enter a valid ingredient
+							
+							const toast = this.toastCtrl.create({
+								message: 'Please enter an ingredient name',
+								duration: 1000,
+								position: 'bottom'
+							});
+							
+							toast.present();
+							
+							return;
 						}
+						
+						(<FormArray>this.recipeForm.get('ingredients')).push(new FormControl(data.ingredient, Validators.required));
+						
+						const toast = this.toastCtrl.create({
+							message: 'Item added',
+							duration: 1000,
+							position: 'bottom'
+						});
+						
+						toast.present();
+						
+						console.log(this.recipeForm);
 					}
-				]
-			});
-		prompt.present();
+				}
+			]
+		});
 	}
 	
 	ngOnInit(){
@@ -89,7 +134,8 @@ export class CreateRecipePage implements OnInit{
 		this.recipeForm = new FormGroup({
 			title: new FormControl(null, Validators.required),
 			description: new FormControl(null, Validators.required),
-			difficulty: new FormControl('Easy', Validators.required)
+			difficulty: new FormControl('Easy', Validators.required),
+			ingredients: new FormArray([])
 		});
 	}
 	
